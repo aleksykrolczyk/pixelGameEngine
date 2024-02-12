c95730bbf4b36a9b8a4346603c1e3de55b2677da
aleksykrolczyk <aleksykrolczyk@yahoo.pl>
2024-02-12 17:10:56 +0100

    Initial Commit

---
+++ PixelGameEngine/PixelGameEngineApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelGameEngineApp.swift
+//  PixelGameEngine
+//
+//  Created by Aleksy Krolczyk on 12/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PixelGameEngineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
